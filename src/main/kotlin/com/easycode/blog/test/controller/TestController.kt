7283a5dccf3d95ed574e55fd85136b3a2a56cd63
EeeasyCode <eeeasycode@gmail.com>
2024-02-20 00:00:38 +0900

    [FEAT] test controller 생성

---
+++ src/main/kotlin/com/easycode/blog/test/controller/TestController.kt
@@ -0,0 +1,12 @@
+package com.easycode.blog.test.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/test")
+    fun test(): String {
+        return "test"
+    }
+}
