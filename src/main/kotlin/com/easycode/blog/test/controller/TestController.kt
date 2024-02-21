71cb0b29b9a96b2117c7ca35e69db2032c6972db
Khan Lee <khan.lee@wclub.co.kr>
2024-02-21 14:22:01 +0900

    [FEAT] get method 테스트

--- src/main/kotlin/com/easycode/blog/test/controller/TestController.kt
+++ src/main/kotlin/com/easycode/blog/test/controller/TestController.kt
@@ -1,6 +1,8 @@
 package com.easycode.blog.test.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -8,5 +10,10 @@ class TestController {
     @GetMapping("/test")
     fun test(): String {
         return "test"
+    }
+
+    @GetMapping("/test/hi")
+    fun index(@RequestParam("name") name: String): ResponseEntity<String> {
+        return ResponseEntity.ok("Hello, $name!")
     }
 }
---
+++ src/main/kotlin/com/easycode/blog/test/service/TestService.kt
@@ -0,0 +1,7 @@
+package com.easycode.blog.test.service
+
+import org.springframework.stereotype.Service
+
+@Service()
+class TestService {
+}
