101f83ba971235922fbddb3681fc5dfea48d931c
Khan Lee <khan.lee@wclub.co.kr>
2024-02-21 18:49:38 +0900

    [FEAT] idk commit

--- src/main/kotlin/com/easycode/blog/test/controller/TestController.kt
+++ src/main/kotlin/com/easycode/blog/test/controller/TestController.kt
@@ -2,6 +2,7 @@ package com.easycode.blog.test.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +16,10 @@ class TestController {
     @GetMapping("/test/hi")
     fun index(@RequestParam("name") name: String): ResponseEntity<String> {
         return ResponseEntity.ok("Hello, $name!")
+    }
+
+    @PostMapping("/test")
+    fun testBody() {
+
     }
 }
